93855cc52ae03440803749f721956f6edc07a885
ctreble <elbertchao@gmail.com>
2018-02-21 12:27:52 -0800

    added server skeleton

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app =  express();
+
+const port = 3000;
+
+const bodyParser = require('body-parser');
+const path = require('path');
+const morgan = require('morgan');
+const router = require('./router/index.js')
+
+app.use(morgan('dev'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+
+// app.use(express.static(path.join(__dirname, '/../client')));
+
+
+app.use(router);
+
+app.listen(port, () => console.log('Listening on port 3000!'));
