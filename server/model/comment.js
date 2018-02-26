df9c8d65be9fee441c13688aa8414cd9e4f2e12f
Elbert Chao <elbertchao@gmail.com>
2018-02-26 10:57:35 -0800

    added modularity for model interaction moved methods to named files and adjusted method for callback async

---
+++ server/model/comment.js
@@ -0,0 +1,40 @@
+const Comment = require("../../db/comment");
+const db = require("../../db");
+
+module.exports = {
+  getComments: (input, callback) => {
+    Comment.find({ photo: input.body.photo }).exec((err, data) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+
+  addComment: (input) => {
+    const newComment = new Comment({
+      user: input.params.username,
+      photoId: input.params.photoId,
+      text: input.body.text,
+    });
+    newComment.save();
+  },
+
+  deleteComment: (input, callback) => {
+    Comment.remove({ photoId: input.params.photoId }, callback);
+  },
+
+  editComment: (input, callback) => {
+    Comment.findOneAndUpdate(
+      { photoId: input.params.photoId },
+      { $set: { text: input.body.text } }
+    ).exec((err, data) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, data);
+      }
+    });
+  }
+};
