ab9e90abf4c86779b74ccb876b6fed4b39891918
Elbert Chao <elbertchao@gmail.com>
2018-02-27 20:29:36 -0800

    add react skeleton to start working on

---
+++ client/src/components/Feed/addComment.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+class AddComment extends React.Component {
+  // function to render input box
+  // input text field
+  // send button (on click call save fuction)
+  // function to render button to click when a user wants
+  // function save Grab data and send post request comment
+  constructor() {
+    super();
+    this.state = {
+      commeting: false,
+    };
+  }
+  renderForm() {
+    return (
+      <div>
+        <textarea ref="newText" defaultValue="Add Comment Here" />
+        <button onClick={this.save}>Submit</button>
+      </div>
+    );
+  }
+
+  renderNormal() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.setState({ commeting: true });
+          }}
+        >
+          Add Comment
+        </button>
+      </div>
+    );
+  }
+
+  save() {
+    // does post add to text add to body?
+    // best way to pass username and photoId?
+    const val = this.refs.newText.value; // grabs value from text area
+    axios
+      .post(`/api/${this.props.username}/${this.props.photoId}/comments`, { text: val })
+      .then((res) => {
+        console.log('Comment Posted');
+        this.setState({ commeting: false });
+      });
+  }
+
+  render() {
+    if (this.state.commeting) {
+      return this.renderForm;
+    }
+    return this.renderNormal;
+  }
+}
+
+export default AddComment;
--- client/src/components/Feed/feed.jsx
+++ client/src/components/Feed/feed.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import Post from './post.jsx';
 
-const Feed = () => (
-  <div>
-    I'm the Feed!
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-  </div>
-);
+class Feed extends React.Component {
+  constructor() {
+    super();
+  }
+  // pass down  username from firebase photoid
+  render() {
+    return (
+      <div>
+        <AddPost username={firebase}/>
+        {this.props.posts.map((post) => {
+          <Post key={post._id} post={post} photoId={post._id} username={firebase}/>;
+        })}
+      </div>
+    );
+  }
+}
+// const Feed = () => (
+//   <div>
+//     I'm the Feed!
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//     <Post />
+//   </div>
+// );
 
 export default Feed;
