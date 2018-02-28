e0647932f8525c6a3f6738abe14a95aa74fc155a
Elbert Chao <elbertchao@gmail.com>
2018-02-27 20:29:36 -0800

    add react skeleton to start working on

--- client/src/components/Feed/commentEntry.jsx
+++ client/src/components/Feed/commentEntry.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
-const CommentEntry = () => (
-  <div>
-    I'm a comment!
-  </div>
-);
+class CommentEntry extends React.Component {
+  constructor() {
+    super();
+  }
+
+  EditComment() {
+    //just delete comment and re add?
+    //more efficent?
+    
+  }
+  deleteComment() {
+    //delete request to remove comment
+    //do anthoer get request to rerender comment
+    axios
+      .delete(`/api/${this.props.username}/${this.props.photoId}/comments`)
+  }
+
+  render() {
+    return (
+      <div>
+        I'm a comment!
+        <div>
+          <h3>{this.comment.user}</h3>
+          <p>{this.comment.text}</p>
+          <p>{this.comment.created}</p>
+        </div>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    );
+  }
+}
 
 export default CommentEntry;
--- client/src/components/Feed/post.jsx
+++ client/src/components/Feed/post.jsx
@@ -1,11 +1,70 @@
 import React from 'react';
 import Comments from './comments'
 
-const Post = () => (
-  <div>
-    I'm a Post!
-      <Comments />
-  </div>  
-)
+class Post extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      showComments: false
+    }
+  }
+  getComments(){
+    //username photoid passed down in props from feed
+    axios
+    .get(`/api/${this.props.username}/${this.props.photoId}/comments`, { text: val })
+    .then((res) => {
+      //do something here to add comments to the store
+    });
+  }
+
+  renderPost (){
+    return (
+      <div>
+          I'm a Post!
+          <img src={this.props.post.url}></p>
+          <div>
+            <h3>{this.props.post.user}</h3>
+            <p>{this.props.post.description}</p>
+            <p>{this.props.post.created}</p>
+            <button OnClick={ ()=> {
+              this.getComments()
+              this.setState({showComments:true})
+              } }>View Comments</button>
+          </div>
+        </div>  
+    )
+
+  }
+  
+
+  renderPostAndComments (){
+    return (
+      <div>
+          I'm a Post!
+          <img src={this.props.post.url}></p>
+          <div>
+            <h3>{this.props.post.user}</h3>
+            <p>{this.props.post.description}</p>
+            <p>{this.props.post.created}</p>
+          </div>
+          <div>
+            <Comments />
+          </div>
+        </div>  
+    )
+
+  
+  }
+
+  render(){
+    //dont render coments till clicked
+    if (this.state.showComments) {
+      return this.renderPost;
+    }
+    return this.renderPostAndComments;
+    
+
+  }
+}
  
-export default Post; 
+export default Post;  
