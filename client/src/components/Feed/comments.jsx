946f022673eb16c8c81ba7b22719afd8c47b330a
Jchen <jcdigital316@gmail.com>
2018-02-28 12:53:04 -0800

    removed readme files + started comment comp

--- client/src/components/Feed/comments.jsx
+++ client/src/components/Feed/comments.jsx
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CommentEntry from './commentEntry.jsx';
-const Comment = () => (
-  <div>
-    <CommentEntry />
-    <CommentEntry />
-    <CommentEntry />
-  </div>
-);
-export default Comment;
-// import React from 'react';
-// import CommentEntry from './commentEntry.jsx';
+import { connect } from 'react-redux';
 
-// class Comment extends React.Component {
-//   constructor() {
-//     super();
-//   }
-//   render() {
-//     //this.props.comments to be replaced from store data
-//     //pass photoId down to add Comment
-//     //grap username from firbase token
-//     return (
-//       <div>
-//         {this.props.comments.map((comment) => {
-//           <CommentEntry key={comment._id} comment={comment} photoId={comments[0].photoId} username={firebase.username}/>
-        
-//         })}
-//         <addComment photoId={comments[0].photoId} username={firebase.username}/>
-//       </div>
-//     );
-//   }
-// }
+class Comment extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    // this.props.comments to be replaced from store data
+    // pass photoId down to add Comment
+    // grap username from firbase token
+    return (
+      <div>
+        {this.props.comments.map((comment) => {
+          <CommentEntry
+            key={comment._id}
+            comment={comment}
+            photoId={comments[0].photoId}
+            username={firebase.username}
+          />;
+        })}
+        <addComment photoId={comments[0].photoId} username={firebase.username} />
+      </div>
+    );
+  }
+}
 
-// export default Comment;
+function mapDispatchToProps(dispatch) {
+  // return bindActionCreators(
+  //   {
+  //     login: actions.login,
+  //   },
+  //   dispatch,
+  // );
+}
+
+function mapStateToProps(state) {
+  // return { currentUser: state.currentUser };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Comment);
