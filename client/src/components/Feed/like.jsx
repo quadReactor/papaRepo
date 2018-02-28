c1c539d5a989c8105d0f454b651753df77cb6203
Elbert Chao <elbertchao@gmail.com>
2018-02-28 08:26:31 -0800

    implemented like component

---
+++ client/src/components/Feed/like.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Like extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+			like: false
+		}
+	}
+	toggleLike () {
+		if(this.state.like){
+			// remove like
+		} else {
+			//add like
+			axios.put(`/api/${this.props.username}/${this.props.photoId}/like`,{})
+		}
+		this.setState({like:!this.state.like})
+	 
+	}
+
+	render () {
+		return (
+			<div>
+				<img onClick={this.toggleLike}  src="this is a like button immage"> 
+				<p>{this.props.likes.length} </p>
+			</div>
+		)
+	}
+
+}
+
+export default Like;
