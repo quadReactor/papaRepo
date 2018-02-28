0bad74396c06b73dbc1d7f0349ddfa782b3bbe86
Elbert Chao <elbertchao@gmail.com>
2018-02-28 13:52:45 -0800

    add skeleton for following page

--- client/src/components/Follow/following.jsx
+++ client/src/components/Follow/following.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Person from './person.jsx';
 
-const Following = () => (
-  <div>
-    Smurfs i'm following
-  </div>  
-)
- 
-export default Following; 
+const Following = () => <div>Smurfs i'm following</div>;
+export default Following;
+
+// Render Following
+
+// class Following extends React.Component {
+//   constructor() {
+//     super();
+//   }
+//   render() {
+//     return (
+//       <div>
+//         {this.props.currentUser.following.map((person) => {
+//           <Person person={person} />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
