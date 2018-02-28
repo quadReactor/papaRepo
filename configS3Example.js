36840a146855b83ad0082ff0cdb2d5e447e9111f
John <yahya.al.mashni@mail.utoronto.ca>
2018-02-28 09:24:22 -0700

    s3 model created

---
+++ configS3Example.js
@@ -0,0 +1,7 @@
+// RENAME THIS configS3Example.js by removing the word "Example" then uncomment  the require in s3 model. configS3.js is gitignored.
+
+module.exports =  {
+  "accessKeyId": "access key id goes here",
+  "secretAccessKey": "secret access key goes here",
+  "region": "us-east-1"
+}
