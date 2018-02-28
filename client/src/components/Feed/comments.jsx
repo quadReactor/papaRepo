1dbdb74fc7848c61155ddec2f73605e75d1fc424
Elbert Chao <elbertchao@gmail.com>
2018-02-27 20:29:36 -0800

    add react skeleton to start working on

--- client/src/components/Feed/comments.jsx
+++ client/src/components/Feed/comments.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import CommentEntry from './commentEntry.jsx';
 
-const Comment = () => (
-  <div>
-    <CommentEntry />
-    <CommentEntry />
-    <CommentEntry />
-  </div>
-);
+class Comment extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    //this.props.comments to be replaced from store data
+    //pass photoId down to add Comment
+    //grap username from firbase token
+    return (
+      <div>
+        {this.props.comments.map((comment) => {
+          <CommentEntry key={comment._id} comment={comment} photoId={comments[0].photoId} username={firebase.username}/>
+        
+        })}
+        <addComment photoId={comments[0].photoId} username={firebase.username}/>
+      </div>
+    );
+  }
+}
+
+// const Comment = () => (
+//   <div>
+//     <CommentEntry />
+//     <CommentEntry />
+//     <CommentEntry />
+//   </div>
+// );
 
 export default Comment;
