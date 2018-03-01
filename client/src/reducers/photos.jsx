404f772c60cfdfbac3bf8fc900371396dde6cdcf
Jack Vaught <johnmvaught@gmail.com>
2018-02-28 17:49:11 -0800

    hardcodes currentUser and photos

--- client/src/reducers/photos.jsx
+++ client/src/reducers/photos.jsx
@@ -1,4 +1,4 @@
-export default (state = ['hi'], action = {}) => {
+export default (state = [], action = {}) => {
   switch (action.type) {
     case 'PHOTOS_RECIEVED':
       return action.payload;
--- client/src/routesMap.js
+++ client/src/routesMap.js
@@ -5,8 +5,9 @@ export default {
     path: '/',
     thunk: async (dispatch, getState) => {
       try {
-        const { username, profilePic, displayname } = await getState().firebaseUser;
-        let userFromDatabase = await axios.get(`/api/${username}/current`);
+        //const { username, profilePic, displayname } = await getState().firebaseUser;
+        // let userFromDatabase = await axios.get(`/api/${username}/current`);
+        let userFromDatabase = await axios.get(`/api/Papa@gmail.com/current`); //hardcode
         if (userFromDatabase.length === 0) {
           userFromDatabase = await createNew(username, profilePic, displayname)
         }
@@ -28,9 +29,9 @@ export default {
     path: '/feed',
     thunk: async (dispatch, getState) => {
       try {
-        const { username } = getState().user;
-        const photos = await axios.get(`/api/${username}/follower`);
-        console.log(photos.data);
+        //const { username } = getState().firebaseUser;
+        // const photos = await axios.get(`/api/${username}/follower`);
+        const photos = await axios.get(`/api/Papa@gmail.com/follower`);
         dispatch({ type: 'PHOTOS_RECIEVED', payload: photos.data });
       } catch (error) {
         console.error(error);
