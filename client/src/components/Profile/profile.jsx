e041d9643a9f2b806d6149ced9273e2b953979aa
Elbert Chao <elbertchao@gmail.com>
2018-03-01 14:27:31 -0800

    profile skeleton

--- client/src/components/Profile/profile.jsx
+++ client/src/components/Profile/profile.jsx
@@ -3,39 +3,38 @@ import { connect } from 'react-redux';
 import UserPost from './userPost.jsx';
 import Navbar from './../Navbar/Navbar.jsx';
 
-const Profile = () =>
-  <div>
-    <Navbar />
-  </div>;
+// const Profile = () =>
+//   <div>
+//     <Navbar />
+//   </div>;
 
+// export default Profile;
 
-export default Profile;
+class Profile extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <br />
+        <br />
+        <br />
+        <h2>Bio</h2>
+        <div>{this.props.currentUser.bio}</div>
+        <h2>Feed</h2>
+        {this.props.photos.map(photo => <UserPost key={photo._id} photo={photo} />)}
+      </div>
+    );
+  }
+}
 
-// class Profile extends React.Component {
-//   constructor() {
-//     super();
-//   }
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser,
+    photos: state.photos,
+  };
+}
 
-//   render() {
-//       <div>
-//         <Navbar />
-//         <br />
-//         <br />
-//         <br />
-//         <h2>Bio</h2>
-//         <div>{this.props.currentUser.bio}</div>
-//         <h2>Feed</h2>
-//         {this.props.photos.map(photo => <UserPost key={photo._id} photo={photo} />)}
-//       </div>
-//     );
-//   }
-// }
-
-// function mapStateToProps(state) {
-//   return {
-//     currentUser: state.currentUser,
-//     photos: state.photos,
-//   };
-// }
-
-// export default connect(mapStateToProps)(Profile);
+export default connect(mapStateToProps)(Profile);
--- client/src/components/Profile/userPost.jsx
+++ client/src/components/Profile/userPost.jsx
@@ -1,85 +1,101 @@
-// import React from 'react';
-// import Comments from './comments';
-// import axios from 'axios';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Comments from '../Feed/Comments.jsx';
+import actions from '../../actions/feed_actions';
 
-// class UserPost extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       showComments: false,
-//     };
-//     this.getComments = this.getComments.bind(this);
-//   }
+class UserPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showComments: false,
+    };
+    this.getComments = this.getComments.bind(this);
+  }
+  getComments(user, photoID) {
+    this.props.getComments(user, photoID);
+  }
 
-//   getComments() {
-//     console.log('i got cliked');
-//     // username photoid passed down in props from feed
-//     // axios.get(`/api/${this.props.username}/${this.props.photoId}/comments`).then(() => {
-//     //   // do something here to add comments to the store
-//     // });
-//   }
+  renderPost() {
+    return (
+      <div>
+        <button>Delete</button> <br />
+        <img src={this.props.photo.photoUrl} />
+        <div>
+          <h3>{this.props.photo.displayname}</h3>
+          <p>{this.props.photo.description}</p>
+          <p>{this.props.photo.created}</p>
+          {/* <Like
+            likes={this.props.photo.like}
+            username={this.props.username}
+            photoId={this.props.photoId}
+          /> */}
+          <button
+            onClick={() => {
+              this.getComments(this.props.currentUser.username, this.props.photo.tempId);
+              {
+                /* this.getComments(this.props.currentUser.username, this.props.photo._id); */
+              }
+              this.setState({ showComments: true });
+            }}
+          >
+            View Comments
+          </button>
+        </div>
+      </div>
+    );
+  }
 
-//   renderPost() {
-//     return (
-//       <div>
-//         <img src={this.props.photo.photoUrl} />>
-//         <div>
-//           <h3>{this.props.photo.displayname}</h3>
-//           <p>{this.props.photo.description}</p>
-//           <p>{this.props.photo.created}</p>
-//           {/* <Like
-//             likes={this.props.photo.like}
-//             username={this.props.username}
-//             photoId={this.props.photoId}
-//           /> */}
-//           <button
-//             onClick={() => {
-//               this.getComments();
-//               this.setState({ showComments: true });
-//             }}
-//           >
-//             View Comments
-//           </button>
-//         </div>
-//       </div>
-//     );
-//   }
+  renderPostAndComments() {
+    return (
+      <div>
+        <img src={this.props.photo.photoUrl} />
+        <div>
+          <h3>{this.props.photo.displayname}</h3>
+          <p>{this.props.photo.description}</p>
+          <p>{this.props.photo.created}</p>
+          {/* <Like
+            likes={this.props.post.like}
+            username={this.props.username}
+            photoId={this.props.photoId}
+          /> */}
+          <button
+            onClick={() => {
+              this.getComments();
+              this.setState({ showComments: false });
+            }}
+          >
+            Hide Comments
+          </button>
+        </div>
+        <div />
+      </div>
+    );
+  }
 
-//   renderPostAndComments() {
-//     return (
-//       <div>
-//         <img src={this.props.photo.photoUrl} />>
-//         <div>
-//           <h3>{this.props.photo.displayname}</h3>
-//           <p>{this.props.photo.description}</p>
-//           <p>{this.props.photo.created}</p>
-//           {/* <Like
-//             likes={this.props.post.like}
-//             username={this.props.username}
-//             photoId={this.props.photoId}
-//           /> */}
-//           <button
-//             onClick={() => {
-//               this.getComments();
-//               this.setState({ showComments: false });
-//             }}
-//           >
-//             Hide Comments
-//           </button>
-//         </div>
-//         <div>
-//           <Comments />
-//         </div>
-//       </div>
-//     );
-//   }
+  render() {
+    // dont render coments till clicked
+    if (!this.state.showComments) {
+      return this.renderPost();
+    }
+    return this.renderPostAndComments();
+  }
+}
 
-//   render() {
-//     // dont render coments till clicked
-//     if (!this.state.showComments) {
-//       return this.renderPost();
-//     }
-//     return this.renderPostAndComments();
-//   }
-// }
-// export default UserPost;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      getComments: actions.getComments,
+    },
+    dispatch,
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser,
+    comments: state.comments,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserPost);
