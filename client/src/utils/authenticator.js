7f189628a3a71b08bc10673149a903f687959691
Jack Vaught <johnmvaught@gmail.com>
2018-03-01 14:25:32 -0800

    fixes login redirect bug

--- client/src/utils/authenticator.js
+++ client/src/utils/authenticator.js
@@ -1,31 +1,44 @@
-import axios from 'axios';
 
-const userKey = Object.keys(window.localStorage)
-  .filter(it => it.startsWith('firebase:authUser'))[0];
 
-const user = userKey ? JSON.parse(localStorage.getItem(userKey)) : undefined;
+const getFirebaseUserFromLocalStorage = () => {
+  const userKey = Object.keys(window.localStorage)
+    .filter(it => it.startsWith('firebase:authUser'))[0];
 
-const userData = !!user && {
-  username: user.email,
-  displayname: user.displayName,
-  profilePic: user.photoURL,
+  const user = userKey ? JSON.parse(localStorage.getItem(userKey)) : undefined;
+
+  const userData = !!user && {
+    username: user.email,
+    displayname: user.displayName,
+    profilePic: user.photoURL,
+  };
+  return userData;
 };
-
 
 export default {
   onBeforeChange: async (dispatch, getState) => {
-    const authUser = getState().firebaseUser;
-    if (!!user && !authUser) {
+    const { firebaseUser } = getState();
+    const { type } = getState().location;
+    let shouldGetFirebaseUserFromLocalStorage = true;
+    if (
+      type === 'HOME' || 
+      type === 'LOGIN' || 
+      type === 'LOGOUT'
+    ) {
+      shouldGetFirebaseUserFromLocalStorage = false;
+    }
+    const data = await getFirebaseUserFromLocalStorage();
+    if (!!data && !firebaseUser && shouldGetFirebaseUserFromLocalStorage) {
       dispatch({
         type: 'RELOAD_USER_STATE_SUCCESS',
-        payload: userData,
+        payload: data,
       });
     }
   },
-  onAfterChange: (dispatch, getState) => {
-    const page = getState().location;
-    const homeAlready = (page.type === 'LOGIN');
-    if (!user && !homeAlready) {
+  onAfterChange: async (dispatch, getState) => {
+    const { type } = getState().location;
+    const homeAlready = (type === 'LOGIN');
+    const data = await getFirebaseUserFromLocalStorage();
+    if (!data && !homeAlready) {
       dispatch({ type: 'LOGIN' });
     }
   },
