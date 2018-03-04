5a4b20b8c0750c932cd706eef42b27cfab9c5ff3
Jack Vaught <johnmvaught@gmail.com>
2018-03-03 19:52:03 -0800

    adds css to follows

--- client/src/components/Follow/follow.jsx
+++ client/src/components/Follow/follow.jsx
@@ -12,19 +12,19 @@ const Follow = () => (
   <div>
     <Navbar />
     <div className={style.body}>
-      <h3>Find</h3>
+      <h3 className={style.header}>Find</h3>
       <AddFollowers />
 
-      <h3>Pending Following</h3>
+      <h3 className={style.header}>Pending Following</h3>
       <Pending />
 
-      <h3>Following</h3>
+      <h3 className={style.header}>Following</h3>
       <Following />
 
-      <h3>Pending Follower Request</h3>
+      <h3 className={style.header}>Pending Follower Request</h3>
       <PendingRequest />
 
-      <h3>Followers</h3>
+      <h3 className={style.header}>Followers</h3>
       <Followers />
     </div>
   </div>
